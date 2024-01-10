f6fec42bb6c330af8eb005cd73292e5c931321cd
Alex Caiedo <alex.caicedo@udla.edu.ec>
2024-01-10 09:27:34 -0500

    2do Comit: Se agrego una carpeta view dinde se creo una vista que presenta el clima actual

---
+++ Views/ClimaActual.xaml.cs
@@ -0,0 +1,37 @@
+using CaicedoRamos_EjemploAPI.Modelo;
+using Newtonsoft.Json;
+
+namespace CaicedoRamos_EjemploAPI.Views;
+
+public partial class ClimaActual : ContentPage
+{
+	public ClimaActual()
+	{
+		InitializeComponent();
+	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		string latitud = Lat.Text;
+        string longitud = Lon.Text;
+
+		if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+		{
+			using (var client = new HttpClient())
+			{ 
+				string url = $"https://api.openweathermap.org/data/2.5/weather?lat="+latitud+"&lon="+longitud+"&appid=b286e406c5e0e3739500ccec54a67709"; 
+				var response = await client.GetAsync(url);
+				if (response.IsSuccessStatusCode) 
+				{ 
+					var json = await response.Content.ReadAsStringAsync();
+                    var clima = JsonConvert.DeserializeObject<Rootobject> (json);
+
+					weatherLabel.Text = clima.weather[0].main;
+					cityLabel.Text = clima.name;
+				}
+			}
+
+		}
+
+    }
+}
