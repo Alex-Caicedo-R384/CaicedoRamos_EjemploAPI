917b03cee1a45d2e3d6da4a10f9e26381fd86a8b
Alex Caiedo <alex.caicedo@udla.edu.ec>
2024-01-11 00:04:48 -0500

    4to Commit: Se agrego para imprimir la velocidad del viento

--- Views/ClimaActual.xaml.cs
+++ Views/ClimaActual.xaml.cs
@@ -12,28 +12,38 @@ public partial class ClimaActual : ContentPage
 
     private async void Button_Clicked(object sender, EventArgs e)
     {
-		string latitud = Lat.Text;
+        string latitud = Lat.Text;
         string longitud = Lon.Text;
 
-		if (Connectivity.NetworkAccess == NetworkAccess.Internet)
-		{
-			using (var client = new HttpClient())
-			{ 
-				string url = $"https://api.openweathermap.org/data/2.5/weather?lat="+latitud+"&lon="+longitud+"&appid=b286e406c5e0e3739500ccec54a67709"; 
-				var response = await client.GetAsync(url);
-				if (response.IsSuccessStatusCode) 
-				{ 
-					var json = await response.Content.ReadAsStringAsync();
-                    var clima = JsonConvert.DeserializeObject<Rootobject> (json);
+        if (string.IsNullOrWhiteSpace(latitud) || string.IsNullOrWhiteSpace(longitud))
+        {
+            await DisplayAlert("Advertencia", "Por favor, ingrese valores válidos para Latitud y Longitud.", "OK");
+            return;
+        }
 
-					weatherLabel.Text = clima.weather[0].main;
-					cityLabel.Text = clima.name;
+        if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = $"https://api.openweathermap.org/data/2.5/weather?lat={latitud}&lon={longitud}&appid=b286e406c5e0e3739500ccec54a67709";
+                var response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var clima = JsonConvert.DeserializeObject<Rootobject>(json);
+
+                    weatherLabel.Text = clima.weather[0].main;
+                    cityLabel.Text = clima.name;
                     countryLabel.Text = clima.sys.country;
-                    temperatureLabel.Text = clima.main.temp+ "°C";
+                    temperatureLabel.Text = clima.main.temp+"";
+                    windSpeedLabel.Text = string.Format("{0} m/s", clima.wind.speed);
                 }
-			}
-
-		}
-
+            }
+        }
+        else
+        {
+            await DisplayAlert("Advertencia", "Se requiere conexión a Internet para consultar el clima.", "OK");
+        }
     }
 }
